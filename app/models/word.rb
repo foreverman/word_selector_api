4ed4ee6494b90a96d75850ad66542838182da9f4
sean <seanliugm@gmail.com>
2018-12-26 12:13:17 +0800

    moved code related to fuzzy search to model from controller

--- app/models/word.rb
+++ app/models/word.rb
@@ -3,4 +3,12 @@ class Word < ApplicationRecord
   pg_search_scope :kinda_spelled_like,
                   :against => :spelling,
                   :using => :trigram
+
+  def self.fuzzy_search(word_input, words_excluded)
+    query = self
+    if words_excluded && words_excluded.any?
+      query = query.where.not(spelling: words_excluded)
+    end
+    query.kinda_spelled_like(word_input).limit(10)
+  end
 end
